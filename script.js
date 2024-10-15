a44607f84e1993b1359054079ff418c1bbd5ae8d
Ahmed <shaikahmed3070@gmail.com>
2024-10-15 21:25:30 +0530

    updated

---
+++ script.js
@@ -0,0 +1,10 @@
+const coin = document.querySelector("#coin");
+const button = document.querySelector("#flip");
+const status = document.querySelector("#status");
+const heads = document.querySelector("#headsCount");
+const tails = document.querySelector("#tailsCount");
+
+let headsCount = 0;
+let tailsCount = 0;
+
+function 
